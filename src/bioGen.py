e4fbb64a62fb8dfe2b63b534f5c0fe061b8f157d
cyberjinmics <37157183+cyberjinmics@users.noreply.github.com>
2018-03-09 16:42:14 +0100

    Add files via upload
    All project files uploaded

---
+++ src/bioGen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from random import *
+import random
+
+def bioGen():
+    bios = [
+        u'The cheap and affordable supercars dealer 🚐 \n'
+        u'A VERIFIED @SHOPBRAID car dealer.🚀 \n'
+        u'Check to see my shopbraid-verified sleek cars @shopbraid 💎',
+
+        u'A bonafide car dealer @SHOPBRAID: '
+        u'The place for the strongest metallic horses. \n'
+        u'See my shopbraid-approved cars @shopbraid 😍😍😍',
+
+        u'No 1 @SHOPBRAID dealer in cars and bikes. 🚚\nSee my stuffs uploaded by shopbraid @shopbraid 😤!!!'
+        ]
+    return random.choice(bios)
